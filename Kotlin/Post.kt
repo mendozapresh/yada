1b7ca5aa228be5624702f6a658841b5b61004346
peejay <146358272+mendozapresh@users.noreply.github.com>
2026-06-28 19:43:32 +0800

    Add files via upload

---
+++ Kotlin/Post.kt
@@ -0,0 +1,13 @@
+// your package name
+
+// Do not modify this file except for the package name
+
+class PostKotlin(
+    val imageId: Int,
+    val datePosted: String,
+    val caption: String?,
+    val location: String?,
+    var liked: Boolean,
+    val username: String,
+    val userImageId: Int) {
+}
